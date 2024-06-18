0b7e2bd3caf59f8de30257d6dcf8ca5fa4264001
Nikola <nikolagunchev69@gmail.com>
2024-06-18 16:29:36 +0300

    Delete products from HTML when clicking delete

--- scirpts/checkout.js
+++ scirpts/checkout.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
-let cartHtml;
+let cartHtml='';
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -14,7 +14,7 @@ cart.forEach((cartItem) => {
     }
   });
   cartHtml += `
-<div class="cart-item-container">
+<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -97,8 +97,12 @@ document.querySelector(".js-order-summary").innerHTML = cartHtml;
 
 document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
-    const productId=link.dataset.productId;
-    removeFromCart(productId)
-    console.log(cart);
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
