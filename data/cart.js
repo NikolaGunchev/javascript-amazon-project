aac96b0851ad09384024bddce59130abb2908c8b
Nikola <nikolagunchev69@gmail.com>
2024-06-18 15:09:51 +0300

    Create HTML for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export let cart=[];
+export let cart=[{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity:2
+},{
+  productId:'jpglb2847e9-5323-403f-b7cf-57fde044a955',
+  quantity:1
+}];
 
 export function addToCart(productId,itemQuantity){
     let matchingItemFromCart;
