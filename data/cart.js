9bd196393d65b38ae7240d776395b862abbbafb1
Nikola <nikolagunchev69@gmail.com>
2024-06-18 16:35:31 +0300

    Use local storage for cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem("cart")) ||
+[
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -8,6 +9,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveToStorage(){
+  localStorage.setItem("cart",JSON.stringify(cart));
+}
 
 export function addToCart(productId, itemQuantity) {
   let matchingItemFromCart;
@@ -25,6 +30,8 @@ export function addToCart(productId, itemQuantity) {
       quantity: itemQuantity,
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +44,5 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
 }
