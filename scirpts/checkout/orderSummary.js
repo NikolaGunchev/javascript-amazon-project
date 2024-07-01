d4992557c78e8c79eec8d9347313015c38283a74
Nikola <nikolagunchev69@gmail.com>
2024-07-01 20:29:39 +0300

    Made changes to the delete function

--- scirpts/checkout/orderSummary.js
+++ scirpts/checkout/orderSummary.js
@@ -124,10 +124,6 @@ import {
         const productId = link.dataset.productId;
         removeFromCart(productId);
   
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
         renderOrderSummary();
         renderPaymentSummary();
       });
