ab3c9e38244e6544a26936367e30b9ac7ab5c576
Nikola <nikolagunchev69@gmail.com>
2024-07-04 10:30:38 +0300

    Made function to calculate the delivery option

--- scirpts/checkout/orderSummary.js
+++ scirpts/checkout/orderSummary.js
@@ -11,6 +11,7 @@ import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {
   deliveryOptions,
   getDeliveryOption,
+  calculateDeliveryDate
 } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
@@ -26,13 +27,10 @@ export function renderOrderSummary() {
     const deliveryOptionId = cartItem.deliveryOptionId;
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-    const dateString = deliveryDate.format("dddd, MMMM D");
     cartHtml += `
   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-          Delivery date: ${dateString}
+          Delivery date: ${calculateDeliveryDate(deliveryOption)}
       </div>
   
       <div class="cart-item-details-grid">
@@ -83,9 +81,6 @@ export function renderOrderSummary() {
   function deliveryOptionsHTML(matchingProduct, cartItem) {
     let html = "";
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-      const dateString = deliveryDate.format("dddd, MMMM D");
 
       const priceString =
         deliveryOption.priceCents === 0
@@ -103,7 +98,7 @@ export function renderOrderSummary() {
               name="${matchingProduct.id}">
               <div>
               <div class="delivery-option-date">
-                  ${dateString}
+                  ${calculateDeliveryDate(deliveryOption)}
               </div>
               <div class="delivery-option-price">
                   ${priceString} Shipping
