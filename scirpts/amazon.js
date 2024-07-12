8dcd47805fc0ce41487ce40e6a73911156a59b18
Nikola <nikolagunchev69@gmail.com>
2024-07-13 00:29:03 +0300

    Convert products into classes

--- scirpts/amazon.js
+++ scirpts/amazon.js
@@ -1,6 +1,5 @@
 import { addToCart, updateCartCheckout } from "../data/cart.js";
 import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
 
 let productsHtmL = "";
 products.forEach((product) => {
@@ -17,14 +16,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="${product.getStarsUrl()}">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
-
+          ${product.getPrice()}
           <div class="product-price">
-            $${formatCurrency(product.priceCents)}
+            
           </div>
 
           <div class="product-quantity-container">
