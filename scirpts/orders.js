72387ec13ee8e13e160579c6e5b3f58adaf28314
Nikola <nikolagunchev69@gmail.com>
2024-07-20 17:21:50 +0300

    Auto generate the HTML for orders page

---
+++ scirpts/orders.js
@@ -0,0 +1,90 @@
+import { orders } from "../data/orders.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { formatCurrency } from "./utils/money.js";
+import { getProduct, loadProductsFetch } from "../data/products.js";
+import { cart } from "../data/cart-class.js";
+
+async function renderOrdersPage() {
+  await loadProductsFetch();
+  let orderHeaderHtml = "";
+
+  orders.forEach((order) => {
+    orderHeaderHtml += `
+    <div class="order-container">
+
+          <div class="order-header">
+            <div class="order-header-left-section">
+              <div class="order-date">
+                <div class="order-header-label">Order Placed:</div>
+                <div>${calculateDate(order.ordertime)}</div>
+              </div>
+              <div class="order-total">
+                <div class="order-header-label">Total:</div>
+                <div>$${formatCurrency(order.totalCostCents)}</div>
+              </div>
+            </div>
+
+            <div class="order-header-right-section">
+              <div class="order-header-label">Order ID:</div>
+              <div>${order.id}</div>
+            </div>
+          </div>
+
+          <div class="order-details-grid js-order-details-${order.id}">
+            ${renderProducts(order)}
+            </div>
+          </div>
+        </div>
+    `;
+  });
+
+  function renderProducts(order) {
+    let html=''
+    order.products.forEach((product) => {
+      const matchingProduct = getProduct(product.productId);
+      html += `
+        <div class="product-image-container">
+            <img src="${matchingProduct.image}">
+        </div>
+
+        <div class="product-details">
+            <div class="product-name">
+                ${matchingProduct.name}
+            </div>
+            <div class="product-delivery-date">
+                Arriving on: ${calculateDate(product.estimatedDeliveryTime)}
+            </div>
+            <div class="product-quantity">
+                Quantity: ${product.quantity}
+            </div>
+            <button class="buy-again-button button-primary">
+                <img class="buy-again-icon" src="images/icons/buy-again.png">
+                <span class="buy-again-message">Buy it again</span>
+            </button>
+        </div>
+
+        <div class="product-actions">
+            <a href="tracking.html">
+                <button class="track-package-button button-secondary">
+                    Track package
+                </button>
+            </a>
+        </div>
+        `;
+    });
+    return html
+  }
+
+  document.querySelector(".js-orders-grid").innerHTML = orderHeaderHtml;
+
+  function calculateDate(rawDate) {
+    let date = dayjs(rawDate);
+    let formated = date.format("MMMM D");
+    return formated;
+  }
+
+  document.querySelector('.js-cart-quantity').innerHTML=cart.updateCartCheckout()
+}
+renderOrdersPage();
+
+
